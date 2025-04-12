951007cad3ae9d686189045f5f26fb16a885545d
BasantYasserr <154337031+BasantYasserr@users.noreply.github.com>
2025-04-12 13:36:37 +0200

    Update ISavedDesignRepository.cs

--- Roomify.GP.Core/Repositories.Contract/ISavedDesignRepository.cs
+++ Roomify.GP.Core/Repositories.Contract/ISavedDesignRepository.cs
@@ -1,15 +1,1 @@
-﻿using Roomify.GP.Core.Entities.AI;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Roomify.GP.Core.Repositories.Contract
-{
-    public interface ISavedDesignRepository
-    {
-        Task AddAsync(SavedDesign entity); 
-        Task<List<SavedDesign>> GetByUserIdAsync(string userId);  
-    }
-}
