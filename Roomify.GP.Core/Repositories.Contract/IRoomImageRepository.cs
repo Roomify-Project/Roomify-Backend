eb471a5be37da65181eb6b3bea9c144876fc85b1
BasantYasserr <154337031+BasantYasserr@users.noreply.github.com>
2025-04-12 13:35:59 +0200

    Update IRoomImageRepository.cs

--- Roomify.GP.Core/Repositories.Contract/IRoomImageRepository.cs
+++ Roomify.GP.Core/Repositories.Contract/IRoomImageRepository.cs
@@ -1,17 +1,1 @@
-﻿using Roomify.GP.Core.Entities.AI.RoomImage;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Roomify.GP.Core.Repositories.Contract
-{
-    public interface IRoomImageRepository
-    {
-        Task AddAsync(RoomImage entity);  // لإضافة صورة جديدة
-        Task<RoomImage> GetByIdAsync(Guid id);  // للحصول على صورة بناءً على الـ ID
-        Task<List<RoomImage>> GetAllAsync();  // للحصول على جميع الصور
-        Task DeleteAsync(Guid id);  // لحذف صورة
-    }
-}
