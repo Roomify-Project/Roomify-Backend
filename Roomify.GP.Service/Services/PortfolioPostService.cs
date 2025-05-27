50c00ec17ef400c6727a380b167956ab113b8467
mina <Mina.Halaka20@gmail.com>
2025-05-27 10:11:47 +0300

    PortfolioPostModification

--- Roomify.GP.Service/Services/PortfolioPostService.cs
+++ Roomify.GP.Service/Services/PortfolioPostService.cs
@@ -1,10 +1,10 @@
 ﻿using Roomify.GP.Core.Entities;
 using Roomify.GP.Core.Repositories.Contract;
 using Roomify.GP.Core.Service.Contract;
+using Roomify.GP.Core.DTOs.PortfolioPost;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Roomify.GP.Service.Services
@@ -12,16 +12,52 @@ namespace Roomify.GP.Service.Services
     public class PortfolioPostService : IPortfolioPostService
     {
         private readonly IPortfolioPostRepository _repo;
+
         public PortfolioPostService(IPortfolioPostRepository repo)
         {
             _repo = repo;
         }
 
+        public async Task<IEnumerable<PortfolioPostResponseDto>> GetAllAsync()
+        {
+            var posts = await _repo.GetAllAsync();
+            return posts.Select(post => MapToResponseDto(post));
+        }
 
-        public async Task<IEnumerable<PortfolioPost>> GetAllAsync() => await _repo.GetAllAsync();
-        public async Task<IEnumerable<PortfolioPost>> GetByUserIdAsync(Guid userId) => await _repo.GetByUserIdAsync(userId);
-        public async Task<PortfolioPost> GetByIdAsync(Guid id) => await _repo.GetByIdAsync(id);
-        public async Task AddAsync(Guid userId, PortfolioPost post) => await _repo.AddAsync(userId, post);
-        public async Task DeleteAsync(Guid id) => await _repo.DeleteAsync(id);
+        public async Task<IEnumerable<PortfolioPostResponseDto>> GetByUserIdAsync(Guid userId)
+        {
+            var posts = await _repo.GetByUserIdAsync(userId);
+            return posts.Select(post => MapToResponseDto(post));
+        }
+
+        public async Task<PortfolioPostResponseDto> GetByIdAsync(Guid id)
+        {
+            var post = await _repo.GetByIdAsync(id);
+            return post == null ? null : MapToResponseDto(post);
+        }
+
+        public async Task AddAsync(Guid userId, PortfolioPost post)
+        {
+            await _repo.AddAsync(userId, post);
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            await _repo.DeleteAsync(id);
+        }
+
+        private PortfolioPostResponseDto MapToResponseDto(PortfolioPost post)
+        {
+            return new PortfolioPostResponseDto
+            {
+                Id = post.Id,
+                ImagePath = post.ImagePath,
+                Description = post.Description,
+                CreatedAt = post.CreatedAt,
+                ApplicationUserId = post.ApplicationUserId,
+                OwnerUserName = post.ApplicationUser?.UserName,
+                OwnerProfilePicture = post.ApplicationUser?.ProfilePicture
+            };
+        }
     }
 }
